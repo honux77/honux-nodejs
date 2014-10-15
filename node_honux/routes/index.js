133e94cb096cff8fd20cac3eea6afc9f07cdb7c0
Hoyoung Jung <k502000@naver.com>
2014-10-16 01:38:51 +0900

    modify index.html

--- node_honux/routes/index.js
+++ node_honux/routes/index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+/*
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+*/
 
 //mongodb test page
 router.get('/userlist', function(req, res) {
